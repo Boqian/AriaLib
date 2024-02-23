5147644837327e9576ad7ae1bf37f2d214bc35ab
wangboqian <wangboqian@DESKTOP-7IU5JCS>
2024-02-23 04:33:00 +0000

    Add project files.

---
+++ AriaLib.cpp
@@ -0,0 +1,14 @@
+﻿// AriaLib.cpp : Defines the entry point for the application.
+//
+
+#include "AriaLib.h"
+
+using namespace std;
+
+#include <fmt/core.h>
+
+int main()
+{
+    fmt::print("Hello World!\n");
+    return 0;
+}
