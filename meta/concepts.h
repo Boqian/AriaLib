177d66bc267db5a8481c5637539dd8a277942996
Boqian <wangboqian@gmail.com>
2024-03-09 00:46:11 +0000

    refine CppProperties.json

--- meta/concepts.h
+++ meta/concepts.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "type_traits.h"
+#include <utility> //std::declval
+
 
 namespace aria {
 /*
 https://en.cppreference.com/w/cpp/header/concepts
 */
-template<class T, class U>
+template <class T, class U>
 concept same_as = is_same_v<T, U> && is_same_v<U, T>;
 
-  
+template <class From, class To>
+concept convertable_to = requires { static_cast<To>(std::declval<From>()); };
 
 
 }
--- meta/tests/test_concepts.cpp
+++ meta/tests/test_concepts.cpp
@@ -3,4 +3,7 @@
 void test_concepts() {
   static_assert(aria::same_as<int, int>);
   static_assert(!aria::same_as<int, double>);
+
+
+
 }
