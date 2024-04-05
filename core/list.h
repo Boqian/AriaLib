27bf2ecc554989d7d4264c14469ad24c48b8716c
Boqian <wangboqian@gmail.com>
2024-04-05 21:25:16 +0100

    fix list::add_node

--- core/list.h
+++ core/list.h
@@ -70,35 +70,90 @@ public:
  // using reverse_iterator = aria::reverse_iterator<iterator>;
  // using const_reverse_iterator = aria::reverse_iterator<const_iterator>;
 
-  list() = default;
+  list() noexcept = default;
 
-  void push_back(value_type &&value) {
-    auto p = create_node(forward<value_type>(value));
-    if (m_last) {
-      m_last->next = p;
-      m_last = p;
-    } else {
-      m_first = m_last = p;
-    }
+  ~list() noexcept { clear(); }
+
+  void push_back(value_type &&value) { add_node(m_last, forward<value_type>(value)); }
+  void push_front(value_type &&value) { add_node(nullptr, forward<value_type>(value)); }
+  void pop_back() noexcept { erase_node(m_last); }
+  void pop_front() noexcept { erase_node(m_first); }
+
+  reference front() noexcept { return m_first->value; } 
+  const_reference front() const noexcept { return m_first->value; } 
+  reference back() noexcept { return m_last->value; }
+  const_reference back() const noexcept { return m_last->value; } 
+
+  bool empty() const noexcept { return m_size == 0; }
+  size_type size() const noexcept { return m_size; }
+
+  void clear() noexcept {
+    while (!empty())
+      pop_back();
   }
 
-  bool empty() const noexcept { 
-      return m_size == 0;
-  }
 
   auto begin() const noexcept { return const_iterator(m_first, nullptr); }
   auto end() const noexcept { return const_iterator(nullptr, m_last); }
  // auto begin() noexcept { return iterator(get(0)); }
  // auto end() noexcept { return iterator(get(m_size)); }
 
+  bool operator==(const list &rhs) const noexcept {
+    if (this == &rhs)
+      return true;
+    if (size() != rhs.size())
+      return false;
+    for (auto p = m_first, q = rhs.m_first; p && q; p = p->next, q = q->next) {
+      if (p->value != q->value)
+        return false;
+    }
+    return true;
+  }
+
 private: 
   using node_type = node<T>;
   using node_allocator_type = typename Allocator:: template rebind_alloc<node_type>;
 
-  node_type *create_node(value_type&& x) {
+  node_type *create_node(value_type &&x) {
     auto p = m_alloc.allocate(1);
     aria::construct_at(p, forward<value_type>(x));
     return p;
+  }
+
+  void add_node(node_type *prev, node_type *p) {
+    if (!prev) {
+      p->next = m_first;
+      m_first = p;
+    } else {
+      p->next = prev->next;
+      p->prev = prev;
+    }
+
+    if (p->prev)
+      p->prev->next = p;
+    if (p->next)
+      p->next->prev = p;
+
+    if (prev == m_last)
+      m_last = p;
+
+    m_size++;
+  }
+
+  void add_node(node_type *prev, value_type &&x) { add_node(prev, create_node(forward<value_type>(x))); }
+
+  void erase_node(node_type *p) {
+    if (p == m_first) {
+      m_first = p->next;
+    } else if (p == m_last) {
+      m_last = p->prev;
+    } else {
+      p->prev->next = p->next;
+    }
+
+    aria::destroy_at(p);
+    m_alloc.deallocate(p, 1);
+    m_size--;
   }
 
   node_type *m_first = nullptr;
--- core/tests/test_list.cpp
+++ core/tests/test_list.cpp
@@ -5,6 +5,32 @@ using namespace aria;
 
 TEST(test_list, base) {
   list<int> li;
+  EXPECT_EQ(li.size(), 0);
+  EXPECT_TRUE(li.empty());
   li.push_back(10);
+  EXPECT_EQ(li.size(), 1);
+  EXPECT_FALSE(li.empty());
   EXPECT_EQ(*li.begin(), 10);
+  EXPECT_EQ(li.front(), 10);
+  EXPECT_EQ(li.back(), 10);
+  li.push_back(20);
+  EXPECT_EQ(*li.begin(), 10);
+  EXPECT_EQ(*(++li.begin()), 20);
+  EXPECT_EQ(li.front(), 10);
+  EXPECT_EQ(li.back(), 20);
+  EXPECT_EQ(li.size(), 2);
+  li.push_front(30);
+  EXPECT_EQ(li.front(), 30);
+  EXPECT_EQ(li.size(), 3);
+  li.pop_back();
+  EXPECT_EQ(li.size(), 2);
+  EXPECT_EQ(li.front(), 30);
+  EXPECT_EQ(li.back(), 10);
+  li.pop_front();
+  EXPECT_EQ(li.size(), 1);
+  EXPECT_EQ(li.front(), 10);
+  EXPECT_EQ(li.back(), 10);
+  li.pop_front();
+  EXPECT_TRUE(li.empty());
 }
+
