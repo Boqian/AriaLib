14520601154d068f4ecc91a642c8a17df4da1531
Boqian <wangboqian@gmail.com>
2024-05-08 22:13:09 +0100

    add more concept

--- core/concepts.h
+++ core/concepts.h
@@ -12,6 +12,23 @@ template <class Derived, class Base>
 concept derived_from = is_base_of_v<Base, Derived> && is_constructible_v<const volatile Derived *, const volatile Base *>;
 
 template <class T>
+concept destructible = is_nothrow_destructible_v<T>;
+
+template <class T, class... Args>
+concept constructible_from = destructible<T> && is_constructible_v<T, Args...>;
+
+template <class T>
+concept move_constructible = constructible_from<T, T> && convertible_to<T, T>;
+
+template <class T>
+concept copy_constructible =
+    move_constructible<T> && constructible_from<T, T &> && convertible_to<T &, T> && constructible_from<T, const T &> &&
+    convertible_to<const T &, T> && constructible_from<T, const T> && convertible_to<const T, T>;
+
+template <class T>
+concept default_initializable = constructible_from<T> && requires { T{}; } && requires { ::new T; };
+
+template <class T>
 concept integral = is_integral_v<T>;
 
 template <class T>
