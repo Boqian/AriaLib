2c5ec25f84f6388393d887cc5b1dd42f985fda18
Boqian <wangboqian@gmail.com>
2024-05-12 16:58:59 +0100

    impl bitset

--- core/bitset.h
+++ core/bitset.h
@@ -8,38 +8,49 @@ template <size_t N> class bitset {
 public:
   class reference {
   public:
-    constexpr operator bool() const noexcept {}
+    constexpr reference(bitset<N> &a_bs, size_t a_pos) : bs(a_bs), pos(a_pos) {}
+    constexpr operator bool() const noexcept { return bs.get(pos); }
+
+    constexpr reference &operator=(bool value) noexcept {
+      bs.set(pos, value);
+      return *this;
+    }
 
   private:
-    bitset<N> &word;
+    bitset<N> &bs;
     size_t pos;
   };
+
+  constexpr bitset() noexcept = default;
+  constexpr bitset(unsigned long val) { v[0] = val; }
+  constexpr bitset(unsigned long long val) { v[0] = val; }
 
   constexpr bitset &reset(size_t i) { return set(i, false); }
 
   constexpr bitset &set(size_t i, bool value = true) {
     auto [words_idx, bit_idx] = get_idx(i);
-    array[words_idx] &= ~(1 << bit_idx);
+    v[words_idx] &= ~(1 << bit_idx);
     if (value) {
-      array[words_idx] |= (1 << bit_idx);
+      v[words_idx] |= (1 << bit_idx);
     }
     return *this;
   }
 
   constexpr bool operator[](size_t i) const { return get(i); }
+  constexpr reference operator[](size_t i) { return reference(*this, i); }
 
   constexpr unsigned long long to_ullong() const {
     if constexpr (N > bits_per_byte * sizeof(unsigned long long)) {
       throw std::overflow_error("overflow");
     } else {
-      return array[0];
+      return v[0];
     }
   }
 
 private:
   constexpr bool get(size_t i) const {
     auto [words_idx, bit_idx] = get_idx(i);
-    return array[words_idx] & (1 << bit_idx);
+    return v[words_idx] & (1 << bit_idx);
   }
 
   constexpr pair<size_t, size_t> get_idx(size_t i) const { return {i / bits_per_word, i % bits_per_word}; }
@@ -48,6 +59,6 @@ private:
   static constexpr ptrdiff_t bits_per_byte = 8;
   static constexpr ptrdiff_t bits_per_word = bits_per_byte * sizeof(T);
   static constexpr ptrdiff_t words = N / bits_per_word + (N % bits_per_word != 0 ? 1 : 0);
-  T array[words] = {};
+  T v[words] = {};
 };
 } // namespace aria
--- core/tests/test_bitset.cpp
+++ core/tests/test_bitset.cpp
@@ -4,14 +4,26 @@
 using namespace aria;
 
 TEST(test_bitset, basic) {
-  bitset<4> bt;
-  EXPECT_EQ(bt.to_ullong(), 0);
-  bt.set(0, true);
-  EXPECT_EQ(bt.to_ullong(), 1);
-  bt.set(1, true);
-  EXPECT_EQ(bt.to_ullong(), 3);
-  bt.set(0, false);
-  EXPECT_EQ(bt.to_ullong(), 2);
-  bt.set(1, false);
-  EXPECT_EQ(bt.to_ullong(), 0);
+  {
+    bitset<4> bt;
+    EXPECT_EQ(bt.to_ullong(), 0);
+    bt.set(0, true);
+    EXPECT_EQ(bt.to_ullong(), 1);
+    bt.set(1, true);
+    EXPECT_EQ(bt.to_ullong(), 3);
+    bt.set(0, false);
+    EXPECT_EQ(bt.to_ullong(), 2);
+    bt.set(1, false);
+    EXPECT_EQ(bt.to_ullong(), 0);
+  }
+  {
+    bitset<4> bt;
+    EXPECT_EQ(bt.to_ullong(), 0);
+    bt[0] = 1;
+    EXPECT_EQ(bt.to_ullong(), 1);
+    bt[1] = 1;
+    EXPECT_EQ(bt.to_ullong(), 3);
+    bt[2] = 1;
+    EXPECT_EQ(bt.to_ullong(), 7);
+  }
 }
