15e36157d3d060bbd747da8156b6b26fce6dba21
Boqian <wangboqian@gmail.com>
2024-05-23 13:23:13 +0100

    Composite type traits

--- core/type_traits.h
+++ core/type_traits.h
@@ -166,12 +166,6 @@ template <class T> struct is_floating_point : bool_constant<is_floating_point_v<
 template <class T> inline constexpr bool is_arithmetic_v = is_integral_v<T> || is_floating_point_v<T>;
 template <class T> struct is_arithmetic : bool_constant<is_arithmetic_v<T>> {};
 
-template <class T> struct is_scalar : bool_constant<is_arithmetic_v<T> || is_enum_v<T> || is_pointer_v<T> || is_null_pointer_v<T>> {};
-template <class T> inline constexpr bool is_scalar_v = is_scalar<T>::value;
-
-template <class T> struct is_object : bool_constant<is_scalar_v<T> || is_array_v<T> || is_union_v<T> || is_class_v<T>> {};
-template <class T> inline constexpr bool is_object_v = is_object<T>::value;
-
 template <class T> constexpr bool is_function_v = !is_const_v<const T> && !is_reference_v<T>;
 template <class T> struct is_function : bool_constant<is_function_v<T>> {};
 
@@ -183,6 +177,20 @@ template <class T> struct _is_member_function_pointer : false_type {};
 template <class T, class U> struct _is_member_function_pointer<T U::*> : bool_constant<is_function_v<T>> {};
 template <class T> struct is_member_function_pointer : _is_member_function_pointer<remove_cv_t<T>> {};
 template <class T> inline constexpr bool is_member_function_pointer_v = is_member_function_pointer<T>::value;
+
+//----------------- Composite type categories -----------------------
+template <class T> constexpr bool is_fundamental_v = is_arithmetic_v<T> || is_void_v<T> || is_null_pointer_v<T>;
+template <class T> struct is_fundamental : bool_constant<is_fundamental_v<T>> {};
+
+template <class T> struct is_compound : bool_constant<is_fundamental_v<T>> {};
+template <class T> inline constexpr bool is_compound_v = is_compound<T>::value;
+
+template <class T> struct is_scalar : bool_constant<is_arithmetic_v<T> || is_enum_v<T> || is_pointer_v<T> || is_null_pointer_v<T>> {};
+template <class T> inline constexpr bool is_scalar_v = is_scalar<T>::value;
+
+template <class T> struct is_object : bool_constant<is_scalar_v<T> || is_array_v<T> || is_union_v<T> || is_class_v<T>> {};
+template <class T> inline constexpr bool is_object_v = is_object<T>::value;
+
 template <class T> constexpr bool is_member_pointer_v = is_member_object_pointer_v<T> || is_member_function_pointer_v<T>;
 template <class T> struct is_member_pointer : bool_constant<is_member_pointer_v<T>> {};
 
