b79b3ed9d819560819a48c7a4b63ea113d1d548b
Boqian <wangboqian@gmail.com>
2024-06-13 21:55:50 +0100

    aria::any copy ctor

--- core/any.h
+++ core/any.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "exception.h"
+#include "type_traits.h"
 #include "unique_ptr.h"
 
 namespace aria {
@@ -12,20 +13,34 @@ public:
 class any {
 public:
   constexpr any() noexcept = default;
+  ~any() = default;
 
-  template <class T> any(T &&value) : m_ptr(make_unique<storage<remove_cvref_t<T>>>(value)) {}
+  any(const any &rhs) {
+    if (rhs.has_value())
+      m_ptr = rhs.m_ptr->clone();
+  }
+
+  template <class T>
+    requires !is_same_v<remove_cvref_t<T>, any>
+             any(T && value) : m_ptr(make_unique<storage<remove_cvref_t<T>>>(value)) {}
 
   constexpr bool has_value() const noexcept { return m_ptr; }
 
   template <class T> friend T any_cast(const any &x);
 
+  constexpr void swap(any &rhs) noexcept { m_ptr.swap(rhs.m_ptr); }
+  constexpr void reset() noexcept { m_ptr.reset(); }
+
 private:
   struct storage_base {
     virtual ~storage_base() = default;
+    virtual unique_ptr<storage_base> clone() const = 0;
   };
 
   template <class T> struct storage : storage_base {
-    storage(T &x) : value(x) {}
+    storage(const T &x) : value(x) {}
+    unique_ptr<storage_base> clone() const override { return make_unique<storage<T>>(value); }
+
     T value;
   };
 
--- core/tests/test_any.cpp
+++ core/tests/test_any.cpp
@@ -4,12 +4,23 @@
 using namespace aria;
 
 TEST(any, basic) {
-  any x = 1;
-  auto y = any_cast<int>(x);
-  EXPECT_EQ(y, 1);
-  try {
-    auto z = any_cast<double>(x);
-  } catch (bad_any_cast ex) {
-    EXPECT_STREQ(ex.what(), "bad any cast");
+  {
+    any x = 1;
+    auto y = any_cast<int>(x);
+    EXPECT_EQ(y, 1);
+    try {
+      auto z = any_cast<double>(x);
+    } catch (bad_any_cast ex) {
+      EXPECT_STREQ(ex.what(), "bad any cast");
+    }
+  }
+}
+
+TEST(any, rule5) {
+  {
+    any x = 100;
+    auto y = x;
+    EXPECT_EQ(any_cast<int>(x), 100);
+    EXPECT_EQ(any_cast<int>(y), 100);
   }
 }
