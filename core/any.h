70baeeca28e9656d4bb133e95926802f658d4144
Boqian <wangboqian@gmail.com>
2024-06-13 22:42:44 +0100

    aria::any::type()

--- core/any.h
+++ core/any.h
@@ -2,6 +2,7 @@
 #include "concepts.h"
 #include "exception.h"
 #include "unique_ptr.h"
+#include <typeinfo>
 
 namespace aria {
 
@@ -24,6 +25,32 @@ public:
     requires not_same<remove_cvref_t<T>, any>
   any(T &&value) : m_ptr(make_unique<storage<remove_cvref_t<T>>>(value)) {}
 
+  template <class T>
+    requires not_same<remove_cvref_t<T>, any>
+  any &operator=(T &&value) {
+    any tmp = forward<T>(value);
+    swap(tmp);
+    return *this;
+  }
+
+  any(any &&rhs) noexcept { swap(rhs); }
+
+  any &operator=(const any &rhs) {
+    if (this != &rhs) {
+      auto tmp = rhs;
+      swap(tmp);
+    }
+    return *this;
+  }
+
+  any &operator=(any &&rhs) noexcept {
+    if (this != &rhs) {
+      auto tmp = move(rhs);
+      swap(tmp);
+    }
+    return *this;
+  }
+
   constexpr bool has_value() const noexcept { return m_ptr; }
 
   template <class T> friend T any_cast(const any &x);
@@ -31,16 +58,19 @@ public:
   constexpr void swap(any &rhs) noexcept { m_ptr.swap(rhs.m_ptr); }
   constexpr void reset() noexcept { m_ptr.reset(); }
 
+  const std::type_info &type() const noexcept { return m_ptr ? m_ptr->type() : typeid(void); }
+
 private:
   struct storage_base {
     virtual ~storage_base() = default;
     virtual unique_ptr<storage_base> clone() const = 0;
+    virtual const std::type_info &type() const = 0;
   };
 
   template <class T> struct storage : storage_base {
     storage(const T &x) : value(x) {}
     unique_ptr<storage_base> clone() const override { return make_unique<storage<T>>(value); }
-
+    const std::type_info &type() const override { return typeid(T); }
     T value;
   };
 
--- core/tests/test_any.cpp
+++ core/tests/test_any.cpp
@@ -6,8 +6,8 @@ using namespace aria;
 TEST(any, basic) {
   {
     any x = 1;
-    auto y = any_cast<int>(x);
-    EXPECT_EQ(y, 1);
+    EXPECT_EQ(x.type(), typeid(int));
+    EXPECT_EQ(any_cast<int>(x), 1);
     try {
       auto z = any_cast<double>(x);
     } catch (bad_any_cast ex) {
@@ -20,7 +20,15 @@ TEST(any, rule5) {
   {
     any x = 100;
     auto y = x;
+    EXPECT_EQ(x.type(), typeid(int));
+    EXPECT_EQ(y.type(), typeid(int));
     EXPECT_EQ(any_cast<int>(x), 100);
     EXPECT_EQ(any_cast<int>(y), 100);
+  }
+  {
+    any x = 100;
+    x = 1.5;
+    EXPECT_EQ(x.type(), typeid(double));
+    EXPECT_DOUBLE_EQ(any_cast<double>(x), 1.5);
   }
 }
