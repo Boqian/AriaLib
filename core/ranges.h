4a6f4472900c9c3c5ecff3bc9153652d559487ae
Boqian <wangboqian@gmail.com>
2024-06-21 20:54:59 +0100

    ranges::size()

--- core/ranges.h
+++ core/ranges.h
@@ -10,18 +10,15 @@ template <class> inline constexpr bool enable_borrowed_range = false;
 namespace _begin {
 void begin() = delete; // Block unqualified name lookup
 
-template <class T>
-concept has_member = requires(T t) {
+template <class T> concept has_member = requires(T t) {
   { t.begin() } -> input_or_output_iterator;
 };
 
-template <class T>
-concept has_adl = has_class_or_enumTpe<T> && requires(T t) {
+template <class T> concept has_adl = has_class_or_enumTpe<T> && requires(T t) {
   { begin(t) } -> input_or_output_iterator;
 };
 
-template <class T>
-concept has_begin = is_array_v<remove_reference_t<T>> || has_member<T> || has_adl<T>;
+template <class T> concept has_begin = is_array_v<remove_reference_t<T>> || has_member<T> || has_adl<T>;
 } // namespace _begin
 
 template <_begin::has_begin T> constexpr auto begin(T &&val) {
@@ -39,18 +36,15 @@ template <class T> using iterator_t = decltype(ranges::begin(declval<T &>()));
 namespace _end {
 void end() = delete; // Block unqualified name lookup
 
-template <class T>
-concept has_member = requires(T t) {
+template <class T> concept has_member = requires(T t) {
   { t.end() } -> sentinel_for<iterator_t<T>>;
 };
 
-template <class T>
-concept has_adl = has_class_or_enumTpe<T> && requires(T t) {
+template <class T> concept has_adl = has_class_or_enumTpe<T> && requires(T t) {
   { end(t) } -> sentinel_for<iterator_t<T>>;
 };
 
-template <class T>
-concept has_end = is_array_v<remove_reference_t<T>> || has_member<T> || has_adl<T>;
+template <class T> concept has_end = is_array_v<remove_reference_t<T>> || has_member<T> || has_adl<T>;
 } // namespace _end
 
 template <_end::has_end T> constexpr auto end(T &&val) {
@@ -63,13 +57,52 @@ template <_end::has_end T> constexpr auto end(T &&val) {
   }
 }
 
-template <class T>
-concept range = requires(T &t) {
+template <class T> concept range = requires(T &t) {
   ranges::begin(t);
   ranges::end(t);
 };
 
 template <range R> using sentinel_t = decltype(ranges::end(declval<R &>()));
+
+template <class Se, class It> inline constexpr bool disable_sized_sentinel_for = false;
+
+template <class Se, class It> concept sized_sentinel_for =
+    sentinel_for<Se, It> && !disable_sized_sentinel_for<remove_cv_t<Se>, remove_cv_t<It>> && requires(const It &i, const Se &s) {
+      { s - i } -> same_as<iter_difference_t<It>>;
+      { i - s } -> same_as<iter_difference_t<It>>;
+    };
+
+namespace _size {
+void size() = delete;
+
+template <class T> concept has_member = requires(T t) {
+  { t.size() } -> integral;
+};
+
+template <class T> concept has_adl = requires(T t) {
+  { size(t) } -> integral;
+};
+
+template <class T> concept can_difference = requires(T t) {
+  { ranges::begin(t) } -> forward_iterator;
+  { ranges::end(t) } -> sized_sentinel_for<decltype(ranges::begin(t))>;
+};
+
+template <class T> concept has_size = is_array_v<remove_reference_t<T>> || has_member<T> || has_adl<T> || can_difference<T>;
+
+} // namespace _size
+
+template <_size::has_size T> auto size(T &&val) {
+  if constexpr (is_array_v<remove_reference_t<T>>) {
+    return extent_v<remove_cvref_t<T>>;
+  } else if constexpr (_size::has_member<T>) {
+    return val.size();
+  } else if constexpr (_size::has_adl<T>) {
+    return size(val);
+  } else if (_size::can_difference<T>) {
+    return ranges::end(val) - ranges::begin(val);
+  }
+}
 
 } // namespace ranges
 } // namespace aria
--- core/tests/test_ranges.cpp
+++ core/tests/test_ranges.cpp
@@ -1,16 +1,26 @@
+#include "list.h"
 #include "ranges.h"
+#include "vector.h"
 #include "gtest/gtest.h"
 
 using namespace aria;
 
-TEST(test_ranges, begin_end) {
-  int x[10]{};
-  auto it = ranges::begin(x);
-  static_assert(is_same_v<decltype(it), int *>);
-  *it = 5;
-  EXPECT_EQ(x[0], 5);
+TEST(test_ranges, begin_end_size) {
+  {
+    int x[10]{};
+    EXPECT_EQ(ranges::size(x), 10);
 
-  auto it2 = ranges::end(x);
-  static_assert(is_same_v<decltype(it2), int *>);
-  EXPECT_EQ(x + 10, it2);
+    auto it = ranges::begin(x);
+    static_assert(is_same_v<decltype(it), int *>);
+    *it = 5;
+    EXPECT_EQ(x[0], 5);
+
+    auto it2 = ranges::end(x);
+    static_assert(is_same_v<decltype(it2), int *>);
+    EXPECT_EQ(x + 10, it2);
+  }
+  {
+    list<int> li = {1, 2, 3, 4};
+    EXPECT_EQ(ranges::size(li), 4);
+  }
 }
